46502602a462ec0012041ecc3848b1d1b17003ec
Shirish Baral <59017772+sireesbrl@users.noreply.github.com>
2024-06-01 18:15:20 +0545

    Update CRUD.py

--- CRUD.py
+++ CRUD.py
@@ -1,5 +1,5 @@
 import psycopg
-
+from getpass import getpass
 
 def add_info():
     while True:
@@ -16,38 +16,69 @@ def add_info():
 
 
 def read_info():
+    #clear screen
     record = cur.execute("""SELECT * FROM person""").fetchall()
+    
     print("ID    First Name     Last Name")
     for row in record:
         print(f"{row[0]}    {row[1]}    {row[2]}")
 
 def update_info():
     read_info()
+    
     choice = int(input("Enter ID: ").strip())
     f_name = input("First name to: ").strip()
     l_name = input("Last name to: ").strip()
+    
     cur.execute(
         """UPDATE person SET First_Name = %s, Last_Name = %s WHERE ID = %d""", (f_name, l_name, choice)
     )
 
 def delete_info():
     read_info()
+    
     choice = int(input("Enter ID: ").strip())
+    
     cur.execute(
         """DELETE FROM person WHERE ID = %d""", (choice)
     )
 
-with psycopg.connect("dbname=postgres user=sirees password=sirees host=localhost") as conn:
-    with conn.cursor() as cur:
-        #try:
-        cur.execute(
-            """CREATE TABLE person(ID serial PRIMARY KEY, First_Name text NOT NULL, Last_Name text NOT NULL)"""
-        )
-        #except psycopg.errors.DuplicateTable:
-            #print("Table 'Person' already exists!")
-        #else:
-            #print("Creating 'Person' table...")
+def connect_db(db, user_name, passwd, host_add, port_no):
+    with psycopg.connect(f"dbname={db} user={user_name} password={passwd} host={host_add} port={port_no}") as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """CREATE TABLE person(ID serial PRIMARY KEY, First_Name text NOT NULL, Last_Name text NOT NULL)"""
+            )
 
-        add_info()
+        print("Table 'Person' created...")
+        print("Options Available")
+        print("1. Add Info \n2. Read Info \n3. Update Info \n4. Delete Info")
+        
+        choice = int(input("Select: ").strip())
+        
+        match choice:
+            case 1:
+                add_info()
+            case 2:
+                read_info()
+            case 3:
+                update_info()
+            case 4:
+                delete_info()
+            case _:
+                pass
+                
         conn.commit()
         conn.close()
+
+def main():
+    db = input("Database: ").strip()
+    user_name = input("Username: ").strip()
+    passwd = getpass()
+    host_add = input("Host: ").strip()
+    port_no = int(input("Port: ").strip())
+    
+    connect_db(db, user_name, passwd, host_add, port_no)
+
+if __name__ == "__main__":
+    main()
