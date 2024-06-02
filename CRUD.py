9ccacba801b277381ea96a18c34c7f8cda331274
Sirees <sireesbrl@gmail.com>
2024-06-02 11:36:41 +0545

    trying class methods

--- CRUD.py
+++ CRUD.py
@@ -1,80 +1,94 @@
-#connecting to a postgresql database and
-#creating a table 'person' and performing
-#various CRUD operations
+#connecting to a postgresql database and creating a table 'person' and performing various CRUD operations
 
 import psycopg
 from getpass import getpass
 
-def add_info():
-    #clear screen
-    while True:
-        f_name = input("Enter first name: ").strip()
-        l_name = input("Enter last name: ").strip()
+class Database:
 
-        cur.execute(
-            """INSERT INTO person(First_Name, Last_Name) VALUES (%s, %s)""", (f_name, l_name)
-        )
+    def __init__(self, db, user_name, passwd, host_add, port_no):
+        self.db = db
+        self.user_name = user_name
+        self.passwd = passwd
+        self.host_add = host_add
+        self.port_no = port_no
 
-        choice = input("Add more? y/n: ").strip()
-        if choice == "n":
-            break
+        with psycopg.connect(f"dbname={self.db} user={self.user_name} password={self.passwd} host={self.host_add} port={self.port_no}") as conn:
+            pass
+            #with conn.cursor() as cur:
+            #    cur.execute(
+            #        """CREATE TABLE person(ID serial PRIMARY KEY, First_Name text NOT NULL, Last_Name text NOT NULL)"""
+            #    )
+            #    print("Table 'Person' created...")
 
 
-def read_info():
-    #clear screen
-    record = cur.execute("""SELECT * FROM person""").fetchall()
-    
-    print("ID    First Name     Last Name")
-    for row in record:
-        print(f"{row[0]}    {row[1]}    {row[2]}")
+    def add_info(self):
+        #clear screen
+        while True:
+            f_name = input("Enter first name: ").strip()
+            l_name = input("Enter last name: ").strip()
 
-def update_info():
-    read_info()
-    
-    choice = int(input("Enter ID: ").strip())
-    f_name = input("First name to: ").strip()
-    l_name = input("Last name to: ").strip()
-    
-    cur.execute(
-        """UPDATE person SET First_Name = %s, Last_Name = %s WHERE ID = %d""", (f_name, l_name, choice)
-    )
+            with psycopg.connect(f"dbname={self.db} user={self.user_name} password={self.passwd} host={self.host_add} port={self.port_no}") as conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        """INSERT INTO person(First_Name, Last_Name) VALUES (%s, %s)""", (f_name, l_name)
+                    )
 
-def delete_info():
-    read_info()
-    
-    choice = int(input("Enter ID: ").strip())
-    
-    cur.execute(
-        """DELETE FROM person WHERE ID = %d""", (choice)
-    )
-
-def connect_db(db, user_name, passwd, host_add, port_no):
-    with psycopg.connect(f"dbname={db} user={user_name} password={passwd} host={host_add} port={port_no}") as conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                """CREATE TABLE person(ID serial PRIMARY KEY, First_Name text NOT NULL, Last_Name text NOT NULL)"""
-            )
-
-        print("Table 'Person' created...")
-        print("Options Available")
-        print("1. Add Info \n2. Read Info \n3. Update Info \n4. Delete Info")
+            choice = input("Add more? y/n: ").strip()
+            if choice == "n":
+                break
         
-        choice = int(input("Select: ").strip())
+
+
+    def read_info(self):
+        #clear screen
+        with psycopg.connect(f"dbname={self.db} user={self.user_name} password={self.passwd} host={self.host_add} port={self.port_no}") as conn:
+                with conn.cursor() as cur:
+                    record = cur.execute("""SELECT * FROM person""").fetchall()
+                    #add proper grid view
+                    print("ID      First Name      Last Name")              
+                    for row in record:
+                        print(f"{row[0]}       {row[1]}          {row[2]}")
+
+    def update_info(self):
+        self.read_info()
         
-        match choice:
-            case 1:
-                add_info()
-            case 2:
-                read_info()
-            case 3:
-                update_info()
-            case 4:
-                delete_info()
-            case _:
-                pass
+        choice = int(input("Enter ID: ").strip())
+        f_name = input("First name to: ").strip()
+        l_name = input("Last name to: ").strip()
+        with psycopg.connect(f"dbname={self.db} user={self.user_name} password={self.passwd} host={self.host_add} port={self.port_no}") as conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        """UPDATE person SET First_Name = %s, Last_Name = %s WHERE ID = %s""", (f_name, l_name, choice)
+                    )
+
+    def delete_info(self):
+        self.read_info()
+        
+        choice = int(input("Enter ID: ").strip())
+        with psycopg.connect(f"dbname={self.db} user={self.user_name} password={self.passwd} host={self.host_add} port={self.port_no}") as conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        """DELETE FROM person WHERE ID = %s""", (choice)
+                    )
+
+def operate_db(db):
+    print("Options Available")
+    print("1. Add Info \n2. Read Info \n3. Update Info \n4. Delete Info")
+    
+    choice = int(input("Select: ").strip())
+    
+    match choice:
+        case 1:
+            db.add_info()
+        case 2:
+            db.read_info()
+        case 3:
+            db.update_info()
+        case 4:
+            db.delete_info()
+        case _:
+            pass
                 
-        conn.commit()
-        conn.close()
 
 def main():
     db = input("Database: ").strip()
@@ -83,7 +97,9 @@ def main():
     host_add = input("Host: ").strip()
     port_no = int(input("Port: ").strip())
     
-    connect_db(db, user_name, passwd, host_add, port_no)
+    database = Database(db, user_name, passwd, host_add, port_no)
+    operate_db(database)
+
 
 if __name__ == "__main__":
     main()
